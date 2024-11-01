63f45de339003dfc4639fdaee64c0542228f33e2
priramdona <137527807+priramdona@users.noreply.github.com>
2024-11-01 21:52:25 +0700

    fix custom role

--- app/Models/CustomRole.php
+++ app/Models/CustomRole.php
@@ -11,17 +11,16 @@ class CustomRole extends SpatieRole
 {
     protected $fillable = ['name', 'guard_name', 'business_id']; // Tambahkan atribut yang diperlukan
 
-    public function setAttribute($key, $value)
+    // Mutator untuk mengatur business_id
+    public function setBusinessIdAttribute($value)
     {
-        // Cek dan tambahkan logika khusus untuk business_id atau team_id
-        if ($key === 'business_id' && !array_key_exists('business_id', $this->attributes)) {
-            $this->attributes['business_id'] = $value;
-        }
-        return parent::setAttribute($key, $value);
+        $this->attributes['business_id'] = $value;
     }
 
+    // Scope untuk mengambil role berdasarkan business_id
     public function scopeForBusiness($query, $businessId)
-{
-    return $query->where('business_id', $businessId);
-}
+    {
+        return $query->where('business_id', $businessId);
+    }
+
 }
