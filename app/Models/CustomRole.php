b48b12ad5dcba5f6e9e508c56ef14466e94209a6
priramdona <137527807+priramdona@users.noreply.github.com>
2024-11-01 22:16:38 +0700

    asd

--- app/Models/CustomRole.php
+++ app/Models/CustomRole.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Role as SpatieRole;
@@ -9,6 +10,8 @@ use Spatie\Permission\Models\Role as SpatieRole;
 
 class CustomRole extends SpatieRole
 {
+    use HasFactory;
+    use HasUuids;
     protected $fillable = ['name', 'guard_name', 'business_id']; // Tambahkan atribut yang diperlukan
 
 
