bb01daf6cba5a6da5509453ba0ef75e0fb2bd40d
priramdona <137527807+priramdona@users.noreply.github.com>
2024-11-02 22:02:15 +0700

    crsftoken

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,6 +15,6 @@ class VerifyCsrfToken extends Middleware
         '/redirects/*',
         '/callbacks/*',
         '/payment-gateways/callback/*',
-        'payment-gateways/callback',
+        'payment-gateways/payment-methods-callback',
     ];
 }
