c498174f19eae856aad58702a46115b6e548ed29
priramdona <137527807+priramdona@users.noreply.github.com>
2024-11-02 17:06:35 +0700

    asd

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -54,13 +54,13 @@ class LoginController extends Controller
     }
 
     protected function authenticated(Request $request, $user) {
-        if ($user->is_active != 1) {
-            Auth::logout();
+        // if ($user->is_active != 1) {
+        //     Auth::logout();
 
-            return back()->with([
-                'account_deactivated' => 'Your account is deactivated! Please contact with Super Admin.'
-            ]);
-        }
+        //     return back()->with([
+        //         'account_deactivated' => 'Your account is deactivated! Please contact with Super Admin.'
+        //     ]);
+        // }
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
