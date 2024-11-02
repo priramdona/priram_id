0d69d935aa134f17fbc80d5a131c663a766d7755
priramdona <137527807+priramdona@users.noreply.github.com>
2024-11-02 21:28:04 +0700

    asd

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/redirects/*',
-        '/callbacks/*'
+        '/callbacks/*',
+        '/payment-gateways/callback/*',
     ];
 }
