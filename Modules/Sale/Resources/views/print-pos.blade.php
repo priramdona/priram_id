855e0b8b3130a3adeabf6f5bc0d6c302cabd5fa4
priramdona <137527807+priramdona@users.noreply.github.com>
2024-11-20 15:31:42 +0700

    asd

--- Modules/Sale/Resources/views/print-pos.blade.php
+++ Modules/Sale/Resources/views/print-pos.blade.php
@@ -139,7 +139,7 @@
                 </tr>
                 <tr style="border-bottom: 0;">
                     <td class="centered" colspan="3">
-                        <img src="data:image/png;base64,{{ $barcode }}" alt="Barcode" style="width: 100px; height: 50px;" />/>
+                        <img src="data:image/png;base64,{{ $barcode }}" alt="Barcode" style="width: 100px; height: 100px;" />/>
                     </td>
                 </tr>
             </tbody>
