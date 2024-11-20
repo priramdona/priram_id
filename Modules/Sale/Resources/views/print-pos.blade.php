f61d6ae622350e880b5d787c1f0c817755cb1aa2
priramdona <137527807+priramdona@users.noreply.github.com>
2024-11-20 15:32:04 +0700

    asd

--- Modules/Sale/Resources/views/print-pos.blade.php
+++ Modules/Sale/Resources/views/print-pos.blade.php
@@ -122,7 +122,7 @@
             @endif
             </tbody>
         </table>
-        <table  border="0" cellpadding="0" cellspacing="0" style="width: 100%; table-layout: fixed; border-collapse: collapse; font-family: Arial, sans-serif;">
+        <table  border="0" cellpadding="0" cellspacing="0" style="width: 90%; table-layout: fixed; border-collapse: collapse; font-family: Arial, sans-serif;">
             <tbody>
                 <tr>
                     <td style="width: 60%; text-align: left;">
