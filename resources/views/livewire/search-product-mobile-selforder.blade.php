1159d4e95043c2205910c4c0c680235f2a402d88
priramdona <137527807+priramdona@users.noreply.github.com>
2024-11-22 21:25:20 +0700

    zindex

--- resources/views/livewire/search-product-mobile-selforder.blade.php
+++ resources/views/livewire/search-product-mobile-selforder.blade.php
@@ -42,7 +42,7 @@
     @if(!empty($query))
         <div wire:click="resetQuery" class="position-fixed w-100 h-100" style="left: 0; top: 0; right: 0; bottom: 0;z-index: 1;"></div>
         @if($search_results->isNotEmpty())
-            <div class="card position-absolute mt-1" style="z-index: 2;left: 0;right: 0;border: 0;">
+            <div class="card position-absolute mt-1" style="z-index: 1050;left: 0;right: 0;border: 0;">
                 <div class="card-body shadow">
                     <ul class="list-group list-group-flush">
                         @foreach($search_results as $result)
