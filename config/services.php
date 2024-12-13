dbaace6438362cf8361e483f00ded1d85407499e
priramdona <137527807+priramdona@users.noreply.github.com>
2024-12-13 19:33:01 +0700

    live

--- config/services.php
+++ config/services.php
@@ -30,12 +30,12 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'xendit' => [
-        // 'key' => 'xnd_production_uuzC6hN2NZsUFbNH4y4E5FmYtFOp4DR420eJY3zyiBBpAj7CylHdPyeDcgv9tp7',
-        // 'public_key' => 'xnd_public_production_D7aFSh8NlkhwjDJqZH73vF1XERlVkus4x0jRhjeKPmaQLIgvJbOv5Uuc7axK2k',
+        'key' => 'xnd_production_uuzC6hN2NZsUFbNH4y4E5FmYtFOp4DR420eJY3zyiBBpAj7CylHdPyeDcgv9tp7',
+        'public_key' => 'xnd_public_production_D7aFSh8NlkhwjDJqZH73vF1XERlVkus4x0jRhjeKPmaQLIgvJbOv5Uuc7axK2k',
         // 'key' => "xnd_development_kUV1T0HvdsGIZjjCB44UAZbf3y0uWdsiljJj8waKePO6Be3OYFsushKpNJHtujZB", //test munggi
         // 'public_key' => 'xnd_public_development_MH_989vykBMlxR6b45ZcoljKGwRxSQS7XxVzR38aw8bJBtcF5Mm79PuP0LBqmh5'//test munggi
-        'key' => "xnd_development_YyGYTQMsAiGf3ca0eRu1fwQ01vYb5sc8sqdVyTGpCfTzSxWgf09TUDPtLYSzbse5", //test primaraharjamulia
-        'public_key' => 'xnd_public_development_lAsH_QDuLpMrFfhVeQicsHSnwTo6ppnIlyzo98r8HGiucEmn6Sxl23MIZPgmZAx'//test primaraharjamulia
+        // 'key' => "xnd_development_YyGYTQMsAiGf3ca0eRu1fwQ01vYb5sc8sqdVyTGpCfTzSxWgf09TUDPtLYSzbse5", //test primaraharjamulia
+        // 'public_key' => 'xnd_public_development_lAsH_QDuLpMrFfhVeQicsHSnwTo6ppnIlyzo98r8HGiucEmn6Sxl23MIZPgmZAx'//test primaraharjamulia
 
     ],
 ];
